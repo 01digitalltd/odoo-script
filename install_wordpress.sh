977d398ffd780e1bee42783d1374c1f193d1646a
feverchy <dickychan810@gmail.com>
2025-02-20 14:34:32 +0800

    Update install_wordpress.sh

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -207,7 +207,7 @@ server {
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         include fastcgi_params;
         
         # FastCGI settings
@@ -289,7 +289,7 @@ server {
         location ~ \.php$ {
             include snippets/fastcgi-php.conf;
             fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
-            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
             include fastcgi_params;
             fastcgi_param HTTPS on;
             fastcgi_param HTTP_X_FORWARDED_PROTO https;
